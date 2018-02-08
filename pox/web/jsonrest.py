cf1cbfd90b482453b89a0e04a88c04a047650c90
Hrish1 <hmail17.95@gmail.com>
2018-02-08 09:35:53 +0530

    CREATED jsonrest.py and ADDED the first api

---
+++ pox/web/jsonrest.py
@@ -0,0 +1,56 @@
+from pox.lib.bottle import Bottle, response
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+import pox.lib.util
+import threading
+import json
+import time
+import os
+import ast
+
+app = Bottle()
+log = core.getLogger()
+stats = None
+port = long(38663)
+
+@app.hook("after_request")
+def enable_cors ():
+	response.headers["Access-Control-Allow-Origin"]="*"
+	response.headers["Access-Control-Allow-Methods"]="PUT,GET,POST,DELETE,OPTIONS"
+	response.headers["Access-Control-Allow-Headers"]="Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+
+
+@app.route("/web/jsonrest/controller")
+def get_controller_information():
+	try:
+		data = {}
+		of = core.components.get("of_01")
+		if of == None:
+			log.error("error obtaining module pox.openflow.of_01")
+			return json.dumps(data)
+		data = {
+				"Address":of.address,				
+				"Port":of.port
+			}
+		return json.dumps(data)
+	
+	except  BaseException, e:
+        	log.error(e.message)
+        	data = {}
+        	return json.dumps(data)
+
+
+
+
+def launch(host = None, port = None):
+	def run():
+		try:
+			kwargs = {"host" : host, "port" : int(port)}
+			app.run(**kwargs)
+		except   BaseException,e:
+			 log.error(e.message)
+
+	thread = threading.Thread(target = run)
+    	thread.daemon = True
+   	thread.start()
+	
